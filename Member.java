5e78c39a8caabd6d8a6154d7164d5a3d772261bd
Octar123 <ranjanassmali123@gmail.com>
2025-08-02 14:46:38 +0530

    borrowBook method added and list of borrowbook is added

--- Member.java
+++ Member.java
@@ -1,20 +1,23 @@
+import java.util.*;
+
 public class Member {
     private int memberId;
     private String name;
+    private List<Book> borrowedBooks;
 
-    public int getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(int memberId) {
+    // Constructor
+    public Member(int memberId, String name){
         this.memberId = memberId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
+        this.borrowedBooks = new ArrayList<>();
     }
+
+    public void borrowBook(Book book){
+        this.borrowedBooks.add(book);
+    }
+
+    //Getters
+    public int getMemberId() { return memberId; }
+    public String getName() { return name; }
+    public List<Book> getBorrowedBooks() { return borrowedBooks; }
 }
